1a33b08ad2fd69e50c0aa7b6f257657bff7d7c44
Gmoreno100 <gmoreno@ithreexglobal.com>
2021-11-01 11:34:39 -0300

    asdas

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ require('./services/mongo')
 require('./services/passport.local');
 
 
-app.set('port',process.env.port ||8080);
+app.set('port',process.env.PORT ||8080);
 app.set('views', path.resolve(__dirname, 'views'));
 
 app.engine('.hbs', exphbs({  //Configuro handlebars
