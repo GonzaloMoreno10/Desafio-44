db61ad8261a6c2afd3b51cffdd65e888ef052723
Gmoreno100 <gmoreno@ithreexglobal.com>
2021-11-01 11:23:33 -0300

    01122021

--- dist/src/services/database.js
+++ dist/src/services/database.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.mySQLDB = exports.sqliteDB = void 0;
+exports.sqliteDB = exports.mySQLDB = void 0;
 
 var _knex = _interopRequireDefault(require("knex"));
 
